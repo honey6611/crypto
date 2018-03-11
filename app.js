b851bc4c5b188b88e5c93e1f8655912446dcf640
honey6611 <honey_6611@yahoo.com>
2018-03-11 23:24:32 +0000

    added crypto currency list

--- app.js
+++ app.js
@@ -17,13 +17,38 @@ app.use(bodyparser.json());
 app.set("view engine","ejs");
 
 app.get("/",function(req, res){
-    res.send("server running");
+    //res.send("server running");
+    request('https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=2000', function (error, response, body) {
+        //console.log('error:', error); // Print the error if one occurred
+        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //console.log('body:', body); // Print the HTML for the Google homepage.
+        
+        res.render('pages/index',{
+            data: JSON.parse(body)
+        });
+      });
+      //var data = {body}
 })
 
+app.get("/about",function(req,res){
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/";
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("local");
+        dbo.collection("Cryptocurrencies").find().toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          res.render('pages/about',{
+            data: result
+            })          
+          db.close();
+        });
+      });    
 
+})
 app.listen(8080,function(err,res){
-    if(err!=''){console.log("cannot launch server" + err)}
-    else{console.log('8080 is the magic port');}
+    console.log('8080 is the magic port')
 });
 
 
